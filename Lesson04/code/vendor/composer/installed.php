61f886b3600eab41529c69d47593f4a8d3cd5fa8
Valerii_Panov <val250640@mail.ru>
2024-07-17 01:11:47 +1000

    les04

---
+++ Lesson04/code/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'app/oop',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2098d6e546f29625a44b9c83d331ee055fc8dc91',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'app/oop' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2098d6e546f29625a44b9c83d331ee055fc8dc91',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
